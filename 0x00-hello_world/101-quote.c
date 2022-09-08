85ef1ec1bd189f52aee97883047aa0b2360b7fb7
holat <holatolorunshola@gmail.com>
2022-09-08 18:55:37 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
   */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char text[] = "and the piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", text);
 	return (1);
 }
