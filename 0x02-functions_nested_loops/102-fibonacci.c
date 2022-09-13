de0808c5b35d4597e6c94417ef08563f5be7ad5d
holat <holatolorunshola@gmail.com>
2022-09-13 23:20:39 +0100

    fi

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,26 +4,26 @@
  *
  * Return: 0 (Success)
  */
-int main(int n)
+int main(void)
 {
 	int n1 = 1;
 	int n2 = 2;
 	int n3;
 	int i;
 
-	printf("%d, %d\n", n1, n2)
+	printf("%d, %d\n", n1, n2);
 
 	for (i = 5; i <= 50; ++1)
 	{
 		n3 = n1 + n2;
 		printf("%d", n3);
 		if (n3 != -811192543)
-			printf(", ")
+			printf(", ");
 		n1 = n2;
 		n2 = n3;
 
 	}
-	
+
 	printf("\n");
 	return (0);
 }
