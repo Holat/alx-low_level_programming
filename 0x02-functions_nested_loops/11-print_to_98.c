5538330e8393c3532830844089c9a24143d5b659
holat <holatolorunshola@gmail.com>
2022-09-13 21:35:40 +0100

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,25 @@
-#include 
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers
+ *
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d\, \n", n);
+			n--;
+		}
+
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d\, \n", n);
+			n++;
+		}
+	}
+}
