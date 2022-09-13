d7e830e281eebd92ec8dbc6c16c93d28cb90d326
holat <holatolorunshola@gmail.com>
2022-09-13 17:58:45 +0100

    adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @int: takes an integer variable
+ *
+ *  Return: result
+ */
+
+int add(int, int)
+{
+	int i, j;
+	int result = i + j;
+
+	return (result);
+}
