213e126052f7f3317b7c1e8d1f0568477cc87799
holat <holatolorunshola@gmail.com>
2022-09-13 15:02:06 +0100

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _abs(int) - Returns absolute value
- * @num - number
+ * _abs - Returns absolute value
+ * @n: number
  *
  * Return: aboslute value
  */
-int _abs(int num)
+int _abs(int n)
 {
-	if (num < 0)
-		num = -num;
-	return (num);
+	if (n < 0)
+		n = -n;
+	return (n);
 }
