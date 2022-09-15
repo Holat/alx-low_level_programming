a0b00eb7859ed374f35e968a11d337fabdbd0e2b
holat <holatolorunshola@gmail.com>
2022-09-15 13:08:50 +0100

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: int variable to save digit
+ *
+ * Return: 1 (digit)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
