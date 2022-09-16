06777f8b12e96a42fd825f285a0cf8cc0672ac69
holat <holatolorunshola@gmail.com>
2022-09-16 01:17:34 +0100

    tri

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,22 +10,18 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for ((j = size - i); j < 0; j--)
+			for ((j = size - i); j > 0; j--)
 			{
 				_putchar(' ');
 			}
 			for (j = 0; j < i; j++)
 			{
 
-				_putchar('0');
+				_putchar('#');
 			}
 			if (i == size)
 			{
@@ -34,4 +30,5 @@ void print_triangle(int size)
 			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
