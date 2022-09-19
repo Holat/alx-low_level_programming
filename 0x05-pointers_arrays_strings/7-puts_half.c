bf75a1517077ab1ec8dd91bb284186d58f499816
holat <holatolorunshola@gmail.com>
2022-09-19 18:06:11 +0100

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: param
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int n = len / 2;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(*(str + i));
+	}
+
+	_putchar('\n');
+}
