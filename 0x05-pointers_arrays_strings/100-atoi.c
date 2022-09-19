98efe0a930508aac7ac599d44d1ac4b34a945064
holat <holatolorunshola@gmail.com>
2022-09-19 23:58:53 +0100

    hack

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,7 @@
 /**
  * _atoi - convert string to an int
  * @s: string param
- * 
+ *
  * Return: coverted string
  */
 
@@ -12,16 +12,14 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do 
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
-	}
-	while (*s++);
+	} while (*s++);
 
 	return (num * sign);
 }
