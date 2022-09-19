e5a28e7b73393e89af54587edc3fb78db22c8e76
holat <holatolorunshola@gmail.com>
2022-09-19 19:00:34 +0100

    second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,13 @@
 void puts_half(char *str)
 {
 	int len = strlen(str);
-	int n = len / 2;
+	int n = (len) / 2;
 	int i = 0;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= len ; i++)
 	{
-		_putchar(*(str + i));
+		if (i >= n)
+			_putchar(*(str + i));
 	}
 
 	_putchar('\n');
