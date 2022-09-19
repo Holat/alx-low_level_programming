f45e8c1a89ac3fefeaf5488fd8289f7cba13ac8b
holat <holatolorunshola@gmail.com>
2022-09-19 13:57:18 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints string
+ * @str: accepts the str
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		_putchar(*(str + i));
+
+	_putchar('\n');
+}
