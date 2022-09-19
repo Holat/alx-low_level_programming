dec3797f57603742aac23d3827e53c87395f4da2
holat <holatolorunshola@gmail.com>
2022-09-19 14:41:38 +0100

    p rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: accepts string param
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i = length;
+
+	while (*s != '\0')
+	{
+		lengt++;
+		s++;
+	}
+
+	for (i = length; i >= 0; i--)
+		_putchar(*(s + i));
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * puts2 - prints odd charcters in a string
+ * @str; param
+ */
+
+void puts2(char *str)
+{
+	int i:
+}
