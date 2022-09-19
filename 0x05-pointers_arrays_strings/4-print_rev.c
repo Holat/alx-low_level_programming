a842e45bd0082158521b365bde1a71a4e526bfac
holat <holatolorunshola@gmail.com>
2022-09-19 16:55:29 +0100

    p rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,7 @@
 
 void print_rev(char *s)
 {
-	int length = 0;
-	int i = length;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
+	int length = strlen(s);
 
 	for (i = length; i >= 0; i--)
 		_putchar(*(s + i));
