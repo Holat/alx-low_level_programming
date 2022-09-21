004bb424118a9a6e4b6af0a190dbf757cb98e4e6
holat <holatolorunshola@gmail.com>
2022-09-21 22:25:34 +0100

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,15 +14,15 @@ char *_strcat(char *dest, char *src)
 	int src_len = strlen(src);
 	int dest_len = strlen(dest);
 	int len = src_len + dest_len + 1; /* 1 is for the null character*/
-	char *s = "0";
+	char *s = (char *)malloc(len * sizeof(char));
 	int i = 0;
 
 	for (i = 0; i < dest_len; i++)
-		s[i] = dest[i];
+		*(s + i) = *(dest + i);
 	for (i = 0; i < src_len; i++)
-		s[dest_len + i] = src[i];
+		*(s + dest_len + i) = *(src + i);
 
-	s[len - 1] = '\0';
+	*(s + (len - 1)) = '\0';
 
 	return (s);
 }
