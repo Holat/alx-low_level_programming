1dd9bba185e4c4566a5c778e216b2b736323d808
holat <holatolorunshola@gmail.com>
2022-09-21 23:44:20 +0100

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
 
-	for (; src[i] != '\0' && i < n; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
 
 	for (; i < n; n++)
