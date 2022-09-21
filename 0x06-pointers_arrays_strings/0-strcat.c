8849eaaa28fe44e688ad352036278755de687ca7
holat <holatolorunshola@gmail.com>
2022-09-21 23:15:15 +0100

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include <string.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -13,18 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int src_len = strlen(src);
-	int dest_len = strlen(dest);
-	int len = src_len + dest_len + 1;
-	char *s = (char *)malloc(len * sizeof(char));
+	int dest_len = 0;
+	
+	while (dest[i++])
+		dest_len++;
 
-	for (i = 0; i < dest_len; i++)
-		*(s + i) = *(dest + i);
+	for (i = 0; src[i]; i++)
+		 dest[dest_len++] = src[i] ;
 
-	for (i = 0; i < src_len; i++)
-		*(s + dest_len + i) = *(src + i);
-
-	*(s + (len - 1)) = '\0';
-
-	return (s);
+	return (dest);
 }
