c4e90d78a3915d22ee35a3b93631da326cea0f72
holat <holatolorunshola@gmail.com>
2022-09-22 18:32:03 +0100

    initial

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,11 @@
 
 char *string_toupper(char *s)
 {
-	for (int i = 0; s[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] > 96 && s[i] <= 123)
 			s[i] -= 32;
 	}
 	return (s);
