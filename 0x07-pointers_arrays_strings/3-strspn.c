684852cf4c15a24929abe58c63f495935c34a3c6
holat <holatolorunshola@gmail.com>
2022-09-26 22:33:42 +0100

    initial

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,9 +16,11 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if (s[i] == accepts[j])
+			if (s[i] == accept[j])
 				break;
 		}
+		if (!accept[j])
+			break;
 	}
 	return (i);
 }
