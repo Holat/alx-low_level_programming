110e7f85f0efc5c44254b678c47efb9ec45f39b8
holat <holatolorunshola@gmail.com>
2022-09-28 20:19:05 +0100

    6 task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,15 +9,26 @@
 
 int is_prime_number(int n)
 {
-	if ((n / 1 == n) && (n / n == 1))
-	{
-		if (n != 1)
-		{
-			return (1);
-		}
-	}
-	else
-	{
+	int i = n / 2;
+
+	if (n <= 1)
 		return (0);
-	}
+	return (prime(n, i));
+}
+
+/**
+ * prime - checks for prime numbers
+ * @n: integer
+ * @i: number to start from
+ *
+ * Return: 1
+ */
+
+int prime(int n, int i)
+{
+	if (i <= 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	return (prime(n, i - 1));
 }
