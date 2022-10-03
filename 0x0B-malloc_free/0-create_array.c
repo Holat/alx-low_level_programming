ffc25999a12984fb14b68eb31e52ec5aa49e3c57
holat <holatolorunshola@gmail.com>
2022-10-03 10:41:07 +0100

    initial

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: the value
+ *
+ * Return: a pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s = malloc(sizeof(char *) * size);
+	int i = 0;
+
+	while (i < size)
+	{
+		*(s + i) = c;
+		i++;
+	}
+
+	if (size == 0)
+		return (NULL);
+	else
+		return (s);
+	free(s);
+}
