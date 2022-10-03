625f860f72befa9c9bb1413e7cabd82cb7e1aee0
holat <holatolorunshola@gmail.com>
2022-10-03 12:22:38 +0100

    initial

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - returns pointer to a newly allocated space
@@ -25,6 +26,6 @@ char *_strdup(char *str)
 		i++;
 	}
 
-	return (s)
+	return (s);
 	free (s);
 }
