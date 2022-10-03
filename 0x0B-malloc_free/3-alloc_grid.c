4b7d09b56cf9e29987817de34840042184e942ca
holat <holatolorunshola@gmail.com>
2022-10-03 22:45:41 +0100

    initial

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,16 +11,39 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *arr;
-
-	arr = malloc(sizeof(int) * (width * height));
-
-	if (arr == NULL)
-		return (NULL);
+	int **arr;
+	int i = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	return (**arr);
-	free(arr);
+	arr = (int **)malloc(sizeof(int *) * height);
+
+	if (arr != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			arr[i] = (int *) malloc(sizeof(int) * width);
+			if (arr[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					arr[i][j] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(arr[i]);
+					i--;
+				}
+				free(arr);
+				return (NULL);
+			}
+		}
+		return (arr);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
