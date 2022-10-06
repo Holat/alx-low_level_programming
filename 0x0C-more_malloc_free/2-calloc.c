69dfc62217b09e17376b4f4bffdf45de220e1f7d
holat <holatolorunshola@gmail.com>
2022-10-06 22:07:32 +0100

    initial

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = (nmemb * size);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
@@ -26,7 +26,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	i = 0;
 	while (i < nmemb)
 	{
-		*(p + i) = 0;
+		p[i] = 0;
 		i++;
 	}
 
