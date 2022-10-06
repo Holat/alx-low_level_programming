8feeaf1e48b648531ecacb1080ee7469a89d8bfb
holat <holatolorunshola@gmail.com>
2022-10-06 22:27:07 +0100

    initial

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,17 +12,18 @@
 int *array_range(int min, int max)
 {
 	int *arr;
-	int i;
+	int i, j;
 
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(*arr) * (max - min + 1));
+	j = (max - min) + 1;
+	arr = malloc(sizeof(int) * j);
 	if (arr == NULL)
 		return (NULL);
 
 	i = 0;
-	while (i <= (max - min))
+	while (i < j)
 	{
 		arr[i] = min;
 		i++, min++;
