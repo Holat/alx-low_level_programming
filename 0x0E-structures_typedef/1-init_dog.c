93f6c585b7b9fc859903d3120f9d58a58df3ebf2
holat <holatolorunshola@gmail.com>
2022-10-10 17:19:11 +0100

    intial

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,6 +11,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (name == NULL || age == NULL || owner == NULL)
+		return NULL;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
