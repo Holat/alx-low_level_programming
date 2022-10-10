acb08cfcb1520172ffa2567ee7dc01f1c453fcdb
holat <holatolorunshola@gmail.com>
2022-10-10 16:59:34 +0100

    initial

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Task on structs and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - list name, owner and age of dog
+ * @name: the name of the dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
