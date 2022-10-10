fbaadd902228b49c65984a1f3c689c1e86d0a10b
holat <holatolorunshola@gmail.com>
2022-10-10 17:36:22 +0100

    intial

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints info about the dog
+ * @d: pointer to the dog struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if(d == NULL)
+		printf("nil\n");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %d\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
