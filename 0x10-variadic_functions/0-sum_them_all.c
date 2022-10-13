254faa85aaaf2f402c97b47291edc818214571e2
holat <holatolorunshola@gmail.com>
2022-10-13 18:14:22 +0100

    initial

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum;
+	unsigned int i;
+	int s;
 
 	va_list ap;
+
+
+	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
 
-	va_start(ap, n);
-
+	i = 0;
 	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
