6302609941dec4ce05cf68bec50cb231fbb20a18
holat <holatolorunshola@gmail.com>
2022-10-25 11:59:57 +0100

    initial

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,11 +9,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t i;
+	unsigned long int i;
 	const listint_t *tmp;
 
 	tmp = h;
 	for (i = 0; tmp != NULL; i++)
-		;
+		tmp = tmp->next;
+
 	return (i);
 }
