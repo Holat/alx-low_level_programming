122330376aba8611f1b4ec441b0aad0441c54b3f
holat <holatolorunshola@gmail.com>
2022-10-26 09:48:59 +0100

    initial

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
-size_t print_listint(const listint_t *h);
+ssize_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -34,7 +34,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H*/
